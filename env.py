dba19ea71ea702c4cfe8c53f3a7fce0f723c1185
Tengku Adrianna <75987338+tengkuadrianna@users.noreply.github.com>
2023-04-24 19:43:10 +0100

    Update env.py

--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 import os
 
-os.environ['CLIENT_ID'] = '328055192660-qfm1kgujkvt18q9d582lhv2ki1udakdo.apps.googleusercontent.com'
-os.environ['SECRET'] = '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'
+os.environ['CLIENT_ID'] = '751999639419-qlr1hed9no55qljdpibtf79dq49k3n4p.apps.googleusercontent.com'
+os.environ['SECRET'] = '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'
 os.environ['API_KEY'] = '171045e255b849179ec783de4041d7d2'
